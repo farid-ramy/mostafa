763dcc075e22836ed210325f89e387eec4bcda85
farid ramy <faridramy2003@gmail.com>
2024-06-24 03:39:22 +0300

    servicesAndShop ejs file formated

--- controllers/home/services.js
+++ controllers/home/services.js
@@ -1,10 +1,10 @@
 import Services from "../../models/service.js";
 
 const services_get = async (req, res) => {
-  res.render("./services.ejs", {
+  res.render("./servicesAndShop.ejs", {
     title: "Services",
     user: null,
-    services: await Services.find(),
+    list: await Services.find(),
   });
 };
 
--- controllers/home/shop.js
+++ controllers/home/shop.js
@@ -1,10 +1,10 @@
 import Product from "../../models/product.js";
 
 const shop_get = async (req, res) => {
-  res.render("./shop.ejs", {
+  res.render("./servicesAndShop.ejs", {
     title: "Shop",
     user: null,
-    products: await Product.find(),
+    items : await Product.find(),
   });
 };
 
